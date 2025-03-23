df4614accb175b09ff5427933cabaff504585488
Igor Stepanov <stipyk1309@gmail.com>
2025-03-23 13:14:58 +0300

    feat: base styles + dummy data

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,33 @@
-<template></template>
+<template>
+	<the-header title="Links Keeper"></the-header>
+	<stored-resources :resources="storedResources"></stored-resources>
+</template>
 
-<script></script>
+<script>
+import StoredResources from './components/stored-resources/StoredResources.vue'
+import TheHeader from './components/layouts/TheHeader.vue'
+
+export default {
+	components: { StoredResources, TheHeader },
+	data() {
+		return {
+			storedResources: [
+				{
+					id: 'official-guide',
+					title: 'Official Guide',
+					description: 'Official Vue.js documentation.',
+					link: 'https://vuejs.org',
+				},
+				{
+					id: 'google',
+					title: 'Google',
+					description: 'Learn to Google',
+					link: 'https://google.com',
+				},
+			],
+		}
+	},
+}
+</script>
 
 <style scoped></style>
---
+++ src/components/stored-resources/StoredResources.vue
@@ -0,0 +1,28 @@
+<template>
+	<ul>
+		<resource-item
+			v-for="res in resources"
+			:key="res.id"
+			:title="res.title"
+			:description="res.description"
+			:link="res.link"
+		></resource-item>
+	</ul>
+</template>
+
+<script>
+import ResourceItem from './ResourceItem.vue'
+
+export default {
+	props: ['resources'],
+	components: { ResourceItem },
+}
+</script>
+
+<style scoped>
+ul {
+	list-style: none;
+	margin: auto;
+	max-width: 40rem;
+}
+</style>
